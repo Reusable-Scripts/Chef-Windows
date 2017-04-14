fe7871da4926571f7372633921b5a0cb9d10dc9d
saikrishnama <mandavasaikrishna91@gmail.com>
2017-04-13 23:58:48 -0400

    Update default.rb

--- cookbooks/java/attributes/default.rb
+++ cookbooks/java/attributes/default.rb
@@ -1,2 +1,4 @@
 default['nexus']['java'] = 'java_jdk-1.8.0.71.zip'
 default['nexus']['7zip'] = '7za-9.20.exe'
+
+default['nexus']['jre_windows_installer'] = 'java/jre/8u51-windows-x64/jre-8u51-windows-x64-x64.exe'
