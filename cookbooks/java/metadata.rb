d0d261b955cff22c56baaaca1eea43cfc3db21cf
saikrishnama <mandavasaikrishna91@gmail.com>
2017-04-13 23:49:15 -0400

    Create metadata.rb

---
+++ cookbooks/java/metadata.rb
@@ -0,0 +1,8 @@
+name             'java'
+maintainer       'sai'
+maintainer_email 'sai'
+license          'All rights reserved'
+description      'Creates java folder and extract the data'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends			 'windows'
