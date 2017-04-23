4f8e7f767fc15b8ce0cee19c780b4517ec5d6bcd
saikrishnama <mandavasaikrishna91@gmail.com>
2017-04-22 21:46:25 -0400

    Create default.rb

---
+++ cookbooks/jdk_windows/attributes/default.rb
@@ -0,0 +1,3 @@
+default['nexus']['java_url'] = "https://url/jdk-8u131-windows-x64.exe"
+default['nexus']['INSTALLDIR'] = "C:\Java\x64\jdk1.8.0_60"
+default['nexus']['INSTALLDIRPUBJRE'] = "C:\Java\x64\jre1.8.0_60"
