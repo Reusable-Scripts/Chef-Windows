bf694d049b70816d8d1b601e62242a584ef7b1c9
R Dama <damarak630@gmail.com>
2017-04-25 11:53:35 -0700

    Update default.rb

--- cookbooks/jdk_windows/recipes/default.rb
+++ cookbooks/jdk_windows/recipes/default.rb
@@ -6,7 +6,6 @@
 #Owner's' : saikrishnaMa ,PranithaR, R Dama(ycx35)
 # All rights reserved - Do Not Redistribute
 #
-
 remote_path = node['nexus']['java_url']
 
 directory 'C:\ChefSoftwares' do
@@ -23,40 +22,37 @@ directory 'C:\Java\x64\jdk1.8.0_60' do
     action :create
 end
 
-
 directory 'C:\Java\x64\jre1.8.0_60' do
     action :create
 end
 
-remote_files 'C:\ChefSoftwares\jdk-8u131-windows-x64.exe' do
+remote_file 'C:\Java\x64\jdk-8u131-windows-x64.exe' do
     source remote_path
     action :create_if_missing
 end
 
-template 'C:\ChefSoftwares\install-jdk.bat' do
-    source 'install-jdk.erb'
-    action :create
-   notifies :run, 'batch[java-install]', :immediately
-end
 
-script_path = C:\\ChefSoftwares\\jdk-8u131-windows-x64.exe
-
-batch "java-install" do
-  cwd 'script_path'
-  code "install-jdk.bat"
+execute "java-install" do
+  cwd 'C:\\ChefSoftwares'
+  command "call install-jdk.bat"
   action :nothing
 end
 
+template 'C:\ChefSoftwares\install-jdk.bat' do
+    source 'install-jdk.bat.erb'
+    action :create
+   notifies :run, 'execute[java-install]', :immediately
+end
 
 template 'C:\ChefSoftwares\java-path.bat' do
-    source 'JAVA-HOME.erb'
+    source 'JAVA-HOME.bat.erb'
     action :create
    notifies :run, 'batch[java-path]', :immediately
 end
 
 batch "java-path" do
-  cwd 'script_path'
-  code "java-path"
+  cwd 'C:\\ChefSoftwares\\'
+  code "java-path.bat"
   action :nothing
 #if the server restart required please uncomment below actions
  # notifies :run, 'reboot[Restart-Computer]', :immediately
