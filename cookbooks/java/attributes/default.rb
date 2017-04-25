6e97e3888c6963622d480f8847f57a2e776c66f7
R Dama <damarak630@gmail.com>
2017-04-25 11:52:18 -0700

    Update default.rb

--- cookbooks/java/attributes/default.rb
+++ cookbooks/java/attributes/default.rb
@@ -1,5 +1,4 @@
-default['nexus']['java'] = 'java_jdk-1.8.0.71.zip'
-default['nexus']['7zip'] = '7za-9.20.exe'
-
-default['nexus']['jre_windows_installer'] = 'java/jre/8u51-windows-x64/jre-8u51-windows-x64-x64.exe'
-default['PackageName']['JavaRuntime'] = 'Java 8 Update 51 (64-bit)'
+#default['nexus']['java'] = 'http://**/artifactory/webapp/#/artifacts/browse/tree/General/pkg/java/jdk-8u101-windows-i586.exe'
+default['nexus']['java_url'] = "file:///C:/Users/rdama/Downloads/jdk-8u131-windows-x64.exe"
+default['nexus']['INSTALLDIR'] = "C:\\Java\\x64\\jdk1.8.0_60"
+default['nexus']['INSTALLDIRPUBJRE'] = "C:\\Java\\x64\\jre1.8.0_60"
