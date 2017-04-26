31d6ccd67efe7a9c19be6b23a48765548c3c321e
R Dama <damarak630@gmail.com>
2017-04-25 22:59:17 -0700

    Update default.rb

--- cookbooks/tomcat_windows/attributes/default.rb
+++ cookbooks/tomcat_windows/attributes/default.rb
@@ -1,4 +1,6 @@
-default['nexus']['tomcat_url'] = ""
+default['tomcat']['tomcat_url'] = "file:///C:/Users/uname/Downloads/apache-tomcat-8.5.14-windows-x64.zip"
+default['tomcat']['7zip'] = "file:///C:/Users/uname/Downloads/7z1604-x64.exe"
+
 default['tomcat']['app_port'] = "8080"
 default['tomcat']['app_shout_port'] = "8005"
 default['tomcat']['Connector_port'] = "8009"
