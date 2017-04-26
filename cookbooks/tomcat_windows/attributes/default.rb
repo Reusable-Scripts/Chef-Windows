49f8782b698a9591a5d5c61382a39b31cde72439
R Dama <damarak630@gmail.com>
2017-04-26 00:50:29 -0700

    Update default.rb

--- cookbooks/tomcat_windows/attributes/default.rb
+++ cookbooks/tomcat_windows/attributes/default.rb
@@ -1,5 +1,5 @@
 default['tomcat']['tomcat_url'] = "file:///C:/Users/uname/Downloads/apache-tomcat-8.5.14-windows-x64.zip"
-default['tomcat']['7zip'] = "file:///C:/Users/uname/Downloads/7z1604-x64.exe"
+default['tomcat']['7zip'] = "file:///C:/Users/uname/Downloads/7z1604-x64.msi"
 
 default['tomcat']['app_port'] = "8080"
 default['tomcat']['app_shout_port'] = "8005"
