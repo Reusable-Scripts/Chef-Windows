45cf5a5dee178e93c31c58c88208b15daecf6ee1
R Dama <damarak630@gmail.com>
2017-04-25 22:58:09 -0700

    Update default.rb

--- cookbooks/tomcat_windows/recipes/default.rb
+++ cookbooks/tomcat_windows/recipes/default.rb
@@ -1,14 +1,15 @@
 #
 # Cookbook Name:: tomcat_windows
 # Recipe:: default
-#Owner's' : R Dama(ycx35),saikrishnama,PranithaR
+#Owner's' : R Dama(ycx356),saikrishnama,PranithaR
 # Copyright 2017, SelfEmployee
 #
 # All rights reserved - Do Not Redistribute
 #
 
-remote_path = node['nexus']['tomcat_url']
-7zip_url = node['nexus']['7zip']
+remote_path = node['tomcat']['tomcat_url']
+zip_url = node['tomcat']['7zip']
+
 directory 'C:\ChefSoftwares' do
     action :create
 end
@@ -21,20 +22,20 @@ directory 'C:\ChefSoftwares\TomcatApp\TomcatService' do
     action :create
 end
 
-remote_files 'C:\ChefSoftwares\TomcatApp\7zip.exe' do
-    source 7zip_url
+remote_file 'C:\ChefSoftwares\TomcatApp\7zip.exe' do
+    source zip_url
     action :create_if_missing
 end
-remote_files 'C:\ChefSoftwares\TomcatApp\tomcat.zip' do
+remote_file 'C:\ChefSoftwares\TomcatApp\tomcat.zip' do
     source remote_path
     action :create_if_missing
     notifies :run, 'execute[tomcat.zip]',  :immediately
 end
 
 
-execute 'tomcat.zip'' do
-  cwd 'C:\ChefSoftwares\TomcatApp''
-  command "7za.exe x C:\ChefSoftwares\TomcatApp\tomcat.zip -oC:\ChefSoftwares\TomcatApp\TomcatService * -r -y"
+execute 'tomcat.zip' do
+  cwd 'C:\\ChefSoftwares\\TomcatApp'
+  command "7za.exe x C:\\ChefSoftwares\\TomcatApp\\tomcat.zip -oC:\\ChefSoftwares\\TomcatApp\\TomcatService * -r -y"
   action :nothing
   notifies :run, 'execute[appfileremove]', :immediately
   
@@ -59,20 +60,19 @@ end
 template 'C:\ChefSoftwares\TomcatApp\TomcatService\conf\tomcat-user.xml' do
     source 'tomcat-user.xml.erb'
     action :create
-    notifies :run, 'bash[run-script]', :immediately
+    notifies :run, 'batch[run-script]', :immediately
 end
 
 template 'C:\ChefSoftwares\TomcatApp\TomcatService\conf\server.xml' do
     source 'server.xml.erb'
     action :create
-    notifies :run, 'bash[run-script]', :immediately
+    notifies :run, 'batch[run-script]', :immediately
 end
 
 
 batch 'run-script' do
-    cwd "C:\ChefSoftwares\TomcatApp\TomcatService\bin"
+    cwd "C:\\ChefSoftwares\\TomcatApp\\TomcatService\\bin"
     command "catalina.bat stop"
     command "catalina.bat start"
     action :nothing
 end
-
