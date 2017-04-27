04fdf802c458599df5982cf7361b05d4e943d765
saikrishnama <mandavasaikrishna91@gmail.com>
2017-04-26 23:37:16 -0400

    Update default.rb

--- cookbooks/tomcat_windows/attributes/default.rb
+++ cookbooks/tomcat_windows/attributes/default.rb
@@ -5,3 +5,24 @@ default['tomcat']['app_port'] = "8080"
 default['tomcat']['app_shout_port'] = "8005"
 default['tomcat']['Connector_port'] = "8009"
 default['tomcat']['app_user'] = "tomcatadmin"
+
+
+
+######################## Tomcat Custom##########
+default['artifactory']['7zip'] = '7za.exe'
+default['artifactory']['TomcatURL'] = 'tomcat-8.zip'
+default['artifactory']['installpath'] = 'D:/ChefArtifactory/packages'
+default['artifactory']['initial'] = 'D:/ChefArtifactory'
+default['artifactory']['zipext']= '7zip.exe'
+default['artifactory']['tomcatpkg'] = 'Tomcat.zip'
+default['artifactory']['sfolder'] = 'Tomcat'
+default['artifactory']['tomcatloc'] = 'D:\ChefArtifactory\packages\Tomcat\tomcat8'
+ 
+default['artifactory']['JDK'] =  'jdk-8u131-windows-x64.exe'
+default['artifactory']['jdkinstall'] = 'install-jdk.bat'
+default['artifactory']['jdkpath'] = 'java-path.bat'
+default['artifactory']['JDKEXT'] = 'jdk-8u131-windows-x64.exe'
+ 
+default['artifactory']['INSTALLDIR'] = "D:/ChefArtifactory/packages/jdk1.8.0_60"
+default['artifactory']['INSTALLDIRPUBJRE'] = "D:/ChefArtifactory/packages/jre1.8.0_60"
+ 
